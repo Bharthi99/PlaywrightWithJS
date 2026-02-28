3f5ce6394a5e871294ed41096c6d0b1340b6058e
Bharthi99 <85490921+Bharthi99@users.noreply.github.com>
2026-02-28 17:54:21 +0530

    Add files via upload

---
+++ PremitiveDataTypes.js
@@ -0,0 +1,13 @@
+
+
+var firstName ="test"
+console.log(typeof(firstName))
+var companyName ="IBM"
+console.log(typeof(companyName))
+var mobileNumber = "465485465"
+console.log(typeof(mobileNumber))
+var isAutomation =true
+console.log(typeof(isAutomation))
+var hasPlaywright
+console.log(typeof hasPlaywright)
+console.log("hi")

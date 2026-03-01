caad810341ec8f82c72d97465bc54d5c60826642
Bharthi99 <85490921+Bharthi99@users.noreply.github.com>
2026-03-01 11:42:20 +0530

    Update and rename premitiveDataTypes.js to premitiveDataTypesAssignment.js

---
+++ premitiveDataTypesAssignment.js
@@ -0,0 +1,24 @@
+
+//Declaring and initiating the variable
+var firstName ="Bharati"
+var companyName ="IBM"
+var mobileNumber = "465485465"
+var isAutomation =true
+var hasPlaywright
+
+
+//Varifying the type of the variable
+console.log(typeof(firstName))
+console.log(typeof(companyName))
+console.log(typeof(mobileNumber))
+console.log(typeof(isAutomation))
+console.log(typeof hasPlaywright)
+
+
+//Printing
+console.log("First name:", firstName)
+console.log("Company name:", companyName)
+console.log("Mobile number:", mobileNumber)
+console.log("Boolean value of isAutomation:", isAutomation)
+console.log("hasPlaywright:", hasPlaywright)
+
